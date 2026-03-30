017d1fd057fc24e1cbf48e00529431aaa4dd85d5
Qiwi303 <rapsdug@gmail.com>
2026-03-30 13:15:21 +0000

    Final cleanup: ignore build and remove from repo

--- project/build/_deps/ftxui-build/CMakeFiles/dom.dir/src/ftxui/dom/flexbox_config.cpp.o.d
+++
@@ -1,4 +0,0 @@
-_deps/ftxui-build/CMakeFiles/dom.dir/src/ftxui/dom/flexbox_config.cpp.o: \
- /home/Ars/Desktop/Monitoring_Computer_Hardware/project/build/_deps/ftxui-src/src/ftxui/dom/flexbox_config.cpp \
- /usr/include/stdc-predef.h \
- /home/Ars/Desktop/Monitoring_Computer_Hardware/project/build/_deps/ftxui-src/include/ftxui/dom/flexbox_config.hpp
